5cd33b882a2e254eceb8417daf764f0c376f7a9a
Alaa-saood85 <Alaa.saood85@outlook.com>
2025-05-31 22:11:16 +0300

    Update First Question.py

--- First Question.py
+++ First Question.py
@@ -1,6 +1,6 @@
 def mango(a, b):
     price = a * b - a//3*b
-    return f'{price} # {a-math.floor(a/3)} mangoes for ${b} per unit = ${price}; {math.floor(a/3)} for free'
+    return f'{price} # {a-a//3} mangoes for ${b} per unit = ${price}; {a//3} for free'
 print (mango(2, 3))
 print (mango(3, 3))
 print (mango(9, 5))
